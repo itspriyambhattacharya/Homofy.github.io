e6b4c98e9e157ad1e63014a9bbb58b362b8bbfb7
Priyam Bhattacharya <priyambhattacharya6@gmail.com>
2021-10-30 21:20:51 +0530

    Added js code for dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
-console.log("Test")
+console.log("Test");
+let propDropLink = document.getElementById('prop-drop-link');
+console.log(propDropLink);
+let propDropMenu = document.querySelector('.nav-ul-dropdown');
+console.log(propDropMenu);
+propDropLink.addEventListener('click', function(e){
+    propDropMenu.classList.toggle('d-none')
+})
