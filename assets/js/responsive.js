414bc5cd9c70ff3952d4fad1b5c7e50dc707e187
Priyam Bhattacharya <priyambhattacharya6@gmail.com>
2021-11-06 22:09:04 +0530

    navbar responsive js

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,1 +1,19 @@
-console.log("Responsive")
+console.log("Responsive");
+let menu = document.querySelector(".menu-icon");
+let menu_close = document.querySelector(".menu-close");
+let nav_ul = document.querySelector('.nav-ul');
+menu.addEventListener('click' , function(e){
+    nav_ul.classList.toggle('navbar-slide-left');
+    menu.setAttribute('style' , 'display: none !important ;');
+    menu_close.setAttribute('style' , 'display: block !important ;')
+})
+
+menu_close.addEventListener('click' , function(e){
+    nav_ul.classList.toggle('navbar-slide-left');
+    menu.setAttribute('style' , 'display: block !important ;');
+    menu_close.setAttribute('style' , 'display: none !important ;')
+})
+
+console.log(menu);
+console.log(menu_close);
+console.log(nav_ul);
